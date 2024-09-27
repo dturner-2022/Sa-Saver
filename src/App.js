4048aeaf12ef30c191ff82ca9f39228ef6487057
Dev <turnerjosh676@yahoo.com>
2024-09-26 22:46:05 -0500

    Removed settings text

--- src/App.js
+++ src/App.js
@@ -1322,9 +1322,7 @@ const App = () =>  {
               value={teamMembers}
               onChange={(e) => setTeamMembers(e.target.value)}
             />
-            <p className="important">Select Background: {teamBg}</p>
 
-            <br></br>
             <button className="btn add" onClick={() => addTeam()}>
               Add Team
             </button>
@@ -1333,9 +1331,6 @@ const App = () =>  {
               !DELETE ALL!
             </button>
 
-            <p className="important">
-              Select 3 characters, a background and "Add Team". Name required.
-            </p>
             <p className="important">
               <span className="important">You have {numOfTeams} team(s)!</span>
             </p>
